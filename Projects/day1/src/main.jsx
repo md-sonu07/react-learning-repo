cabb7fac98ec60adf13a62763feb3a815393df2d
md-sonu07 <mdsonu824153@gmail.com>
2025-02-03 12:57:38 +0530

    adding day1 project

---
+++ Projects/day1/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Header from './Components/Header.jsx'
+import Navebar from './Components/Navebar.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <Header />
+      <Navebar />
+      <App />
+    </StrictMode>,
+)
