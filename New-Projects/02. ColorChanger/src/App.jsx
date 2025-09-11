eb7e1c83b7b8c30b3b415ac8127e847fb3214871
md-sonu07 <mdsonu824153@gmail.com>
2025-09-11 10:42:56 +0530

    adding project 2 bg color changer

---
+++ New-Projects/02. ColorChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import ColorBar from "./components/ColorBar";
+// import ArrowBar from "./components/ArrowBar";
+function App() {
+
+  return (
+    <>
+      {/* <ArrowBar/> */}
+      <ColorBar />
+    </>
+  )
+}
+
+export default App
---
+++ New-Projects/02. ColorChanger/src/components/ColorBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+function ColorBar() {
+    const [color, setColor] = useState('black');
+    return (
+        <>
+            <main
+                style={{ backgroundColor: color }}
+                className='h-screen flex justify-center items-center '>
+                <div>
+                    <h1 className='text-2xl font-bold text-white'>CHANGE COLOR 🎨 {color.toLocaleUpperCase()}</h1>
+                </div>
+                <div 
+                className='flex justify-center items-end absolute bottom-0  bg-white rounded-xl p-4 mb-10 gap-4 font-medium'>
+                    <button
+                        onClick={() => setColor('red')}
+                        className='bg-red-500 p-2 rounded-xl text-white hover:bg-red-600 transition-all duration-300 cursor-pointer'>Red</button>
+                    <button
+                        onClick={() => setColor('green')}
+                        className='bg-green-500 p-2 rounded-xl text-white hover:bg-green-600 transition-all duration-300 cursor-pointer'>Green</button>
+                    <button
+                        onClick={() => setColor('blue')}
+                        className='bg-blue-500 p-2 rounded-xl text-white hover:bg-blue-600 transition-all duration-300 cursor-pointer'>Blue</button>
+                    <button
+                        onClick={() => setColor('yellow')}
+                        className='bg-yellow-500 p-2 rounded-xl text-white hover:bg-yellow-600 transition-all duration-300 cursor-pointer'>Yellow</button>
+                    <button
+                        onClick={() => setColor('orange')}
+                        className='bg-orange-500 p-2 rounded-xl text-white hover:bg-orange-600 transition-all duration-300 cursor-pointer'>Orange</button>
+                    <button
+                        onClick={() => setColor('pink')}
+                        className='bg-pink-500 p-2 rounded-xl text-white hover:bg-pink-600 transition-all duration-300 cursor-pointer'>Pink</button>
+                    <button
+                        onClick={() => setColor('purple')}
+                        className='bg-purple-500 p-2 rounded-xl text-white hover:bg-purple-600 transition-all duration-300 cursor-pointer'>Purple</button>
+                    <button
+                        onClick={() => setColor('indigo')}
+                        className='bg-indigo-500 p-2 rounded-xl text-white hover:bg-indigo-600 transition-all duration-300 cursor-pointer'>Indigo</button>
+                    <button
+                        onClick={() => setColor('teal')}
+                        className='bg-teal-500 p-2 rounded-xl text-white hover:bg-teal-600 transition-all duration-300 cursor-pointer'>Teal</button>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default ColorBar
